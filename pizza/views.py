50c4e1306b7cda2b77c11d26e8329184c4f6fd1b
marcins1 <rondusiowy@gmail.com>
2019-09-12 14:17:25 +0200

    Projekt Django i aplikacja Pizza

---
+++ pizza/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    # return HttpResponse("Witaj!")
+    return render(request, 'pizza/index.html')
+
+
+def news(request):
+    return HttpResponse("<h1>Nowości w barze</h1>")
+
+
+def widok(request):
+    return HttpResponse("<h1>Widok</h1>")
